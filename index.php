13eb9e75ebd687f3890b3a4eb506c87f1ea14665
curseb100d <libyahan123@gmail.com>
2024-10-28 09:32:35 +0800

    Doge 1.0.0

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Link to CSS -->
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Top Navigation -->
+    <a href="#" class="logo">
+        <img src="" alt="">Doge Dog Food Store
+    </a>
+    <a href="#">Home</a>
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#" style="float: right">Logout</a>
+</body>
+</html>
